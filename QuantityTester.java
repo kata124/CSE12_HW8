13b6ee0ff9e0ce355878f1b3d6929ebc279b50b2
Elliot Humphrey <evhumphr@ucsd.edu>
2014-05-23 00:47:21 -0700

    Added QuantityTester.java

---
+++ QuantityTester.java
@@ -0,0 +1,59 @@
+/*
+ * CSE 12 Homework 8
+ * Elliot Humphrey / Kenichi Katayama
+ * cs12edl / cs12edu
+ * A09307269 / A09244911
+ * Section A00 (for both)
+ * TODO: DATE
+ */
+
+import junit.framework.TestCase;
+
+
+/**
+ * Title: class QuantityTester
+ * Description: JUnit (4) test class for Quantity
+ * @author Elliot Humphrey / Kenichi Katayama
+ * @version 1.0
+ */
+public class QuantityTester extends TestCase {
+
+	
+	/* TEST 3 constructors */
+	/** Test 0-arg */
+	// creates quantity of value 1 and no units
+	
+	/** Test 1-arg */
+	// 1: takes Quantity as parameter,
+	// makes deep copy of it
+	
+	/** Test 3-arg */
+	// 1: double (value)
+	// 2: List<String> numerator
+	// 3: List<String> denominator
+	
+	
+	/* TEST Quantity METHODS */
+	
+	/** Test mul */
+	
+	/** Test div */
+	
+	/** Test add */
+	
+	/** Test sub */
+	
+	/** Test negate */
+	
+	/** Test normalizedUnit */
+	
+	/** Test normalize */
+	
+	/** Test pow */
+	
+	/** Test equals */
+	
+	/** Test hashCode */
+	
+	
+}
